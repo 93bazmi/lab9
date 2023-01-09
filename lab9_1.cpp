7f9e4e9a94d18a76af11516c471316b334de0d75
bazmi <bambamboom234@gmail.com>
2023-01-09 12:58:43 +0700

    lab9_1.cpp completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -6,26 +6,15 @@ int main()
   char rank;
   cout << "Input your rank: ";
   cin >> rank;
-  if(rank=='S'){
+  if(rank=='S')
     cout << "You have received Super Ultra Rare Unit!!!\n" ;
+  if(rank=='A' || rank=='S')
     cout << "You have received 5 gems.\n";
+  if(rank=='B'|| rank=='S'||rank=='A')
     cout << "You have received 1 gems.\n";
+  if(rank=='C'||rank=='S'||rank=='A'||rank=='B')
     cout << "You have received 2000 coins.\n";
+  if(rank=='D'||rank=='S'||rank=='A'||rank=='B'||rank=='C')
     cout << "You have received very KAK items.\n" ;
-  }else if(rank=='A'){
-    cout << "You have received 5 gems.\n";
-    cout << "You have received 1 gems.\n";
-    cout << "You have received 2000 coins.\n";
-    cout << "You have received very KAK items.\n" ;
-  }else if(rank=='B'){
-    cout << "You have received 1 gems.\n";
-    cout << "You have received 2000 coins.\n";
-    cout << "You have received very KAK items.\n" ;
-  }else if(rank=='C'){
-    cout << "You have received 2000 coins.\n";
-    cout << "You have received very KAK items.\n" ;
-  }else if(rank=='D'){
-    cout << "You have received very KAK items." ;
-  }
   return 0;
 }
